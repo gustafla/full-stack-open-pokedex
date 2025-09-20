d34370e9021c005ace71328140fb3083a11e543e
Lauri Gustafsson <lauri.gustafsson@iki.fi>
2025-09-20 10:33:37 +0300

    e2e-tests/pokedex.spec.js: Add pokemon nav test

--- e2e-tests/pokedex.spec.js
+++ e2e-tests/pokedex.spec.js
@@ -6,4 +6,9 @@ describe('Pokedex', () => {
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
   })
+  test('navigate to ivysaur and check ability', async ({ page }) => {
+    await page.goto('')
+    await page.getByText('ivysaur').click()
+    await expect(page.getByText('chlorophyll')).toBeVisible()
+  })
 })
